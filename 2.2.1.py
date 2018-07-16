9f991aad721dc7396f7b1956503c420726c1d74e
Max <s3tis@ya.ru>
2018-07-16 13:43:31 +0300

    Add files via upload

---
+++ 2.2.1.py
@@ -0,0 +1,10 @@
+x = 0
+while x == 0:
+        a = int(input())
+        if a < 10:
+                continue
+        elif a > 100:
+                break
+        else:
+                pass
+        print(a)
