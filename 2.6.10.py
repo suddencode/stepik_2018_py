dba27ec35e9acb6706bae6a579a909fc2265b745
Max <s3tis@ya.ru>
2018-12-11 16:17:35 +0300

    -

--- 2.6.10.py
+++ 2.6.10.py
@@ -1,16 +1,18 @@
-w, my_list_1 = 0, []
-while w == 0:
-    row_input = input().split()
-    if row_input == ['end']:
-        w = 666
-        break
-    for i in range(len(row_input)):
-        row_input[i] = int(row_input[i])
-    my_list_1.append(row_input)
-for i in range (len(my_list_1)):
-    if i > 0:
-        print(end='\n')
-    for j in range (len(my_list_1[0])):
-        print(my_list_1[i][(j-1)%len(my_list_1[i])] + my_list_1[i][(j+1)%len(my_list_1[i])] + my_list_1[(i-1)%len(my_list_1)][j] + my_list_1[(i+1)%len(my_list_1)][j], end='')
-        if len(my_list_1[i]) > j+1:
-            print(' ', end='')
+n = int(input())
+a = [[0] * n for q in range(n)]
+i, j, x, k = 0, 0, 1, 0
+while x <= n*n:
+    a[i][j] = x
+    if i != j:
+        a[j][i] = (a[k][k] + (n-k*2)*2) * 2 - 4 - x
+    if j != n-k-1:
+        j += 1 
+    elif i != n-k-1:
+        i += 1
+    elif x != n*n:
+        k += 1
+        i = j = k
+        x = a[k][k-1]
+    x += 1
+for row in a:
+    print(' '.join([str(elem) for elem in row]))
